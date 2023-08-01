aa96a285f3ca1a5e9679c80314d5d3e40cf72f90
Enoch Abiodun <g00380316@atu.ie>
2023-08-01 21:20:44 +0100

    Added Constructor and methods to get Price , ID and name

--- src/main/java/ie/atu/shopping_cart/Item.java
+++ src/main/java/ie/atu/shopping_cart/Item.java
@@ -1,4 +1,24 @@
 package ie.atu.shopping_cart;
 
+import lombok.Getter;
+
 public class Item {
+    @Getter
+    private String name;
+    private double price;
+    private int ID;
+
+    public Item(String name, double price, int ID) {
+        this.name = name;
+        this.price = price;
+        this.ID = ID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getID(){
+        return ID;
+    }
 }
