4d47cd57a7da32fafed09452dd23b6a05163b130
Enoch Abiodun <g00380316@atu.ie>
2023-08-01 21:15:33 +0100

    Item test ID method added

--- src/test/java/ie/atu/shopping_cart/ItemTest.java
+++ src/test/java/ie/atu/shopping_cart/ItemTest.java
@@ -18,13 +18,19 @@ class ItemTest {
 
     @Test
     public void testGetName() {
-        Item item = new Item("Iphone 10", 300.00);
+        Item item = new Item("Iphone 10", 349.99,10);
         assertEquals("Iphone 10", item.getName(), "Item name should match");
     }
 
     @Test
     public void testGetPrice() {
-        Item item = new Item("Ipad", 599.99);
+        Item item = new Item("Ipad", 599.99,54);
         assertEquals(599.99, item.getPrice(), "Item price should match");
+    }
+
+    @Test
+    public void testGetID(){
+        Item item = new Item("Apple Watch",299.99,42);
+        assertEquals(42,item.getID(), "Item ID should match" );
     }
 }
